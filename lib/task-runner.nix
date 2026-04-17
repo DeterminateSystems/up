6f70780a1fab6ad5c41ac54defc8f2e5e914ff67
Luc Perkins <lucperkins@gmail.com>
2026-04-17 13:51:54 -0300

    Remove --list output

--- lib/task-runner.nix
+++ lib/task-runner.nix
@@ -304,7 +304,6 @@ let
               ${caseArms}
               *)
                 gum style --foreground ${errorColor} "Unknown task: $1"
-                echo "Run '${config.name} --list' to see available tasks"
                 exit 1
                 ;;
             esac
